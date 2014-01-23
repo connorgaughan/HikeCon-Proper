4627fd827ee5829109c682b5e7dab1e97cb8c87e
Marvin Cespedes <marvin@mcespo.com>
2014-01-23 12:58:42 -0600

    Added suggestions for the site layout
    Added suggestions to the DOM model of the site. It’s still worth
    examining a bit further, but this puts us in a better position.

--- front-page.php
+++ front-page.php
@@ -1,4 +1,112 @@
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' ) ); ?>
+
+<!-- I'm going to do a really rough idea of what the framework should be like by my best interpretation of HTML standards -->
+
+<header id="sticky-dates"> <!-- although this took me a while to debate, it made sense to use this as our header as the site does not have a traditional placement for the nav within the header - also the header is sticky - also the header does not have the site name (which it should). Point is, I need there to be a proper header and having the event dates is not quite appropriate but still structurally functional -->
+	<ul>
+		<li>San Fran ... dates</li>
+		<li>Chicago ... dates</li>
+	</ul>
+</header>
+
+<nav> <!-- not sure where to best place the nav considering that it's somewhat hidden and that our header is going to be sticky. I'll let you dictate proper placement for this. -->
+	<ul>
+		<li></li>
+	</ul>
+</nav>
+
+<article id="red-title">
+	<h1>Hikecon.com</h1>
+</article>
+
+<article id="purple-what-is-hike">
+	<h2>What is Hike</h2>
+	<p>lorem ipsum...</p>
+	<ul>
+		<li>Purpose &amp; Goals</li>
+		<li>Why should I go?</li>
+	</ul>
+</article>
+
+<section> <!-- you can either add the ID or CLASS to the either the section or article tag below. -->
+	<article id="blue-san-francisco"> <!-- you can choose to keep this article or only leave it as a section - if there is content on here however (which there is), I chose to use an article tag. Even so, I'm not entirely sold on the idea. I prefer less tags all the time over more tags -->
+		<hgroup> <!-- this isn't entirely required but this just so happens to be a good eample to showcase where an hgroup can be used -->
+			<h2>San Fran</h2>
+			<h3>April 4</h3>
+		</hgroup>
+		<p>portfolio<br>
+		registration</p>
+		<h3>april 5</h3>
+		<p> <!-- lists can also work for this areat istead of a p tag -->
+		.... Post party</p>
+		button
+		yellow content
+	</article>
+</section>
+
+<section> <!-- you can either add the ID or CLASS to the either the section or article tag below. -->
+	<article id="green-chicago"> <!-- you can choose to keep this article or only leave it as a section - if there is content on here however (which there is), I chose to use an article tag. -->
+		<hgroup> <!-- this isn't entirely required but this just so happens to be a good eample to showcase where an hgroup can be used -->
+			<h2>San Fran</h2>
+			<h3>April 4</h3>
+		</hgroup>
+		<p>portfolio<br>
+		registration</p>
+		<h3>april 5</h3>
+		<p> <!-- lists can also work for this area istead of a P tag -->
+		.... Post party</p>
+		button
+		yellow content
+	</article>
+</section>
+
+<article id="orange-sponsors">
+	<h2>Sponsors</h2>
+	<figure> <!-- ho ho ho, it's a figure tag. figures are used to illustrate images, codeblocks, graphs and charts. I used a figure to house all the images. I actually am not 100% certain that it should be a single figure per image. if that's the case, it's way too much code for my linking. In this example, I use one figure to house all logo images. -->
+		<img src="logo" title="" alt="">
+		<figurecaption>logo caption</figurecaption><!-- dont bother if there is not going to be captions, just alt text the image if that's the case. -->
+	
+		<img src="logo" title="" alt="">
+		<figurecaption>logo caption</figurecaption><!-- dont bother if there is not going to be captions, just alt text the image if that's the case. -->
+	
+		<img src="logo" title="" alt="">
+		<figurecaption>logo caption</figurecaption><!-- dont bother if there is not going to be captions, just alt text the image if that's the case. -->
+	
+		<img src="logo" title="" alt="">
+		<figurecaption>logo caption</figurecaption><!-- dont bother if there is not going to be captions, just alt text the image if that's the case. -->
+	
+		<img src="logo" title="" alt="">
+		<figurecaption>logo caption</figurecaption><!-- dont bother if there is not going to be captions, just alt text the image if that's the case. -->
+	
+		<img src="logo" title="" alt="">
+		<figurecaption>logo caption</figurecaption><!-- dont bother if there is not going to be captions, just alt text the image if that's the case. -->
+	
+		<img src="logo" title="" alt="">
+		<figurecaption>logo caption</figurecaption><!-- dont bother if there is not going to be captions, just alt text the image if that's the case. -->
+	
+		<img src="logo" title="" alt="">
+		<figurecaption>logo caption</figurecaption><!-- dont bother if there is not going to be captions, just alt text the image if that's the case. -->
+	</figure>
+</article>
+
+<article class="standard-white"> <!-- this can be a class as the items added under here - like volunteers would pull from this style -->
+	<h2>Board</h2>
+	<p>or list or whatever of the members</p>
+</article>
+
+<article class="standard-white">
+	<h2>Something else we might add in the future - like volunteers or something</h2>
+	<p>lorem ipsum
+</article>
+
+
+<!-- 
+
+Again I didn't want to mess with what you did. Keep in mind that what I provided above is mostly a suggestion. A valid suggestion as it closely follows web standards but it's not like I've ever been wrong before. I'd say this is mostly 80% accurate. 
+A good way to think about this is that you need to use the term "DIV" as little as possible. Most of your items should sit in an article, section, aside, figure etc. 
+
+-->
+
 <section id="home" class="scroll">
 	<div class="container">
 		<div class="colored full red">
