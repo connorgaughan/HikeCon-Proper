d0aeb73435d4308ab50445f6663b5db8688ac891
Connor Gaughan <connor@clubhousecreative.com>
2014-01-25 10:52:47 -0600

    Sponsors Update

--- parts/frontpage/sponsors.php
+++ parts/frontpage/sponsors.php
@@ -4,11 +4,31 @@
 			<h3>Sponsors</h3>
 		</header>
 		<div class="content">
+			<ul class="tier-one">
 			<?php query_posts(array( 'post_type' => 'sponsors', 'tiers' => 'tier-one'));  while (have_posts ()): the_post(); ?> 
 						
-				<a href="<?php print the_permalink(); ?>"><?php the_post_thumbnail(); ?></a> 
+				<li><a href="<?php print the_permalink(); ?>"><?php the_post_thumbnail(); ?></a></li>
 								
 			<?php endwhile; ?>
+			</ul>
+			<?php wp_reset_query(); ?>
+
+			<ul class="tier-two">
+			<?php query_posts(array( 'post_type' => 'sponsors', 'tiers' => 'tier-two'));  while (have_posts ()): the_post(); ?> 
+						
+				<li><a href="<?php print the_permalink(); ?>"><?php the_post_thumbnail(); ?></a></li>
+								
+			<?php endwhile; ?>
+			</ul>
+			<?php wp_reset_query(); ?>
+
+			<ul class="tier-three">
+			<?php query_posts(array( 'post_type' => 'sponsors', 'tiers' => 'tier-three'));  while (have_posts ()): the_post(); ?> 
+						
+				<li><a href="<?php print the_permalink(); ?>"><?php the_post_thumbnail(); ?></a></li>
+								
+			<?php endwhile; ?>
+			</ul>
 			<?php wp_reset_query(); ?>
 		</div>
 	</div>
